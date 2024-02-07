715a9bee217252cd4c3fe12821cb73f9fbae67d7
spektro26 <132245593+spektro26@users.noreply.github.com>
2024-02-07 12:26:58 -0500

    commit 1

---
+++ ejemplo1.kt
@@ -0,0 +1,6 @@
+fun main() {
+    // Imprimir los números del 1 al 5
+    for (i in 1..5) {
+        println(i)
+    }
+}
---
+++ ejemplo2.kt
@@ -0,0 +1,8 @@
+fun main() {
+    var contador = 1
+    // Imprimir los números del 1 al 5 usando un bucle while
+    while (contador <= 5) {
+        println(contador)
+        contador++
+    }
+}
